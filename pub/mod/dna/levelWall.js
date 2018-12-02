c026080d8ce3a6d549db9b2d66d0d13644902251
Anatolii Yakushko <shaddyx@gmail.com>
2018-12-02 21:09:25 +0200

    [-] fixed walls

--- pub/mod/dna/levelWall.js
+++ pub/mod/dna/levelWall.js
@@ -1,36 +1,42 @@
-let LevelWall = function(dat) {
+// @depends(dna/Sprite)
+let LevelWall = function(st) {
+    dna.Sprite.call(this, st);
     this.Z = 20
     this.w = 1
     this.h = 1
     this.aw = 1
     this.ah = 1
-    this.status = 'wall'
+    this.status = Math.random() > 0.2? 'wall': "torch";
 
-    sys.augment(this, dat)
-};
-
-LevelWall.prototype.evo = function(dt) {
-};
-
-LevelWall.prototype.draw = function() {
-
-    res.tileset.draw(11, this.x-0.5, this.y-0.5, 1, 1);
-
-    if (env.debug) {
-        // debug - draw border and active frames
-        //ctx.lineWidth = 0.01
-        //ctx.strokeStyle = '#ff0000'
-        //ctx.strokeRect(this.x-this.w/2, this.y-this.h/2, this.w, this.h)
-
-        if (this.status) {
-            ctx.fillStyle = '#FF0000'
-            ctx.font = '0.1px kenney-rocket-square'
-            ctx.textAlign = 'center'
-            ctx.textBaseline = 'bottom'
-
-            ctx.fillText(this.status, this.x, this.y)
-        }
+    if (this.status === "wall"){
+        this.tiles = res.wall;
+        this.startTilex = 0;
+        this.endTilex = 0
+    } else {
+        this.tiles = res.torch;
+        this.startTilex = 0;
+        this.endTilex = 3
     }
+    this.framerate = 4
+
 };
+
+sys.extend(LevelWall, dna.Sprite);
+
+// LevelWall.prototype.draw = function() {
+//
+//     res.wall.draw(0, this.x-0.5, this.y-0.5, 1, 1);
+//     //
+//     // if (env.debug) {
+//     //     if (this.status) {
+//     //         ctx.fillStyle = '#FF0000'
+//     //         ctx.font = '0.1px kenney-rocket-square'
+//     //         ctx.textAlign = 'center'
+//     //         ctx.textBaseline = 'bottom'
+//     //
+//     //         ctx.fillText(this.status, this.x, this.y)
+//     //     }
+//     // }
+// };
 
 module.exports = LevelWall;
