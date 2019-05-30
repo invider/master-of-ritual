e8fec25d907089278b079145a3f2ffaf5cd55b29
Igor Khotin <khotin@gmx.com>
2019-05-30 14:25:19 +0300

    [-] removed old content

--- pub/ext/dna/hud/Component.js
+++
@@ -1,9 +0,0 @@
-// component trait
-let Component = {
-    x: 0,
-    y: 0,
-    w: 0,
-    h: 0,
-}
-
-module.exports = Component
--- pub/jam/lib/arrayTools.js
+++
@@ -1,6 +0,0 @@
-var arrayTools = {
-    randomElement: function(items){
-        return items[Math.floor(Math.random() * items.length)]
-    }
-}
-module.exports = arrayTools;
